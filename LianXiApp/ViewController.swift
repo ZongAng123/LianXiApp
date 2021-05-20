f70eb79ef404679f7ade4ebd4ffb238d4e8f57da
纵昂 <zongang15@yahoo.com>
2021-05-20 10:02:58 +0800

    Initial Commit

---
+++ LianXiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LianXiApp
+//
+//  Created by 纵昂 on 2021/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
